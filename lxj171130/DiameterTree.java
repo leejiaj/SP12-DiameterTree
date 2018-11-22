253dcb787357923c6a0f244cce7b3b11cd18b7a6
Leejia James <leejiaj@gmail.com>
2018-11-21 18:30:14 -0600

    starter code

---
+++ lxj171130/DiameterTree.java
@@ -0,0 +1,16 @@
+package lxj171130;
+
+import rbk.Graph;
+
+public class DiameterTree {
+
+	int diameter(Graph g) {
+		//TODO
+		return 0;
+	}
+	
+	public static void main(String args[]) {
+		
+	}
+	
+}
